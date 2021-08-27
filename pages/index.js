f96b524d3a04391a6000cd8f60374c3882610313
shahpriyansh04 <shah.priyansh04@gmail.com>
2021-08-27 21:35:45 +0530

    TP

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2"></div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 xp-2"></div>
   );
 }
