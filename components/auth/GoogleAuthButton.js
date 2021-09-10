22039a9c4bd76e7eb77b71c1ee9607b856d4acd2
shahpriyansh04 <shah.priyansh04@gmail.com>
2021-09-10 19:27:13 +0530

    no message

--- components/auth/GoogleAuthButton.js
+++ components/auth/GoogleAuthButton.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 
-function GoogleAuthButton({ text, signIn }) {
+function GoogleAuthButton({ text }) {
   return (
-    <div
-      onClick={signIn}
-      className="flex text-xl px-4 py-3 border-2 text-center justify-center cursor-pointer rounded-full items-center space-x-4 mt-6 active:scale-95 transition duration-200 ease-out"
-    >
+    <div className="flex text-xl px-4 py-3 border-2 text-center justify-center cursor-pointer rounded-full items-center space-x-4 mt-6 active:scale-95 transition duration-200 ease-out">
       <FcGoogle />
       <p>{text}</p>
     </div>
