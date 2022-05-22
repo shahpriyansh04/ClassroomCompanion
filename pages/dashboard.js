22a6cfedf50b4d65dab9d764a3ead9e780e5c84d
shahpriyansh04 <46381434+shahpriyansh04@users.noreply.github.com>
2022-05-22 10:29:58 +0530

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -29,14 +29,7 @@ function Dashboard() {
     });
   };
 
-  useEffect(() => {
-    if (!user) {
-      router.push("/login");
-    } else {
-      // const { data, isLoading, error } = getClassrooms(user?.id);
-      changeState(data, isLoading, error);
-    }
-  }, [data]);
+
   return (
     <div className="flex  h-screen font-serif bg-light-primary">
       {/* <SideBar /> */}
@@ -48,32 +41,10 @@ function Dashboard() {
             <p className="text-3xl text-center " onClick={changeState}>
               Your Dashboard
             </p>
-            {!classRoomData?.isLoading ? (
-              <div className=" flex-grow  flex flex-col justify-center items-center space-y-6">
-                <Loader
-                  type="TailSpin"
-                  color="#000000"
-                  height={50}
-                  width={50}
-                  visible={true}
-                />
-              </div>
-            ) : !classRoomData?.data.data.length > 0 ? (
+          
               <EmptyDashboard />
-            ) : (
-              <div className="container  flex h-80 flex-grow mt-6 overflow-scroll scrollbar-hide mb-6">
-                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  flex-grow  mx-6 my-4 items-start ">
-                  {classRoomData?.data.data.map(({ id, name, students }) => (
-                    <ClassroomCard
-                      id={id}
-                      name={name}
-                      students={students}
-                      key={id}
-                    />
-                  ))}
-                </div>
-              </div>
-            )}
+      
+          
           </div>
         </div>
       </div>
